8f7b728ba9a41e41ce39c5888e813edfc8e89d45
harshithav5 <harshithav5600@gmail.com>
2020-08-23 16:23:45 +0530

    Merge branch 'edits' of C:\Users\arunv\AndroidStudioProjects\Find-Yourself with conflicts.

--- FindYourself/app/src/main/java/com/findyourself/MainActivity.java
+++ FindYourself/app/src/main/java/com/findyourself/MainActivity.java
@@ -1,7 +1,6 @@
 package com.findyourself;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("Saketha", "Harshitha");
+
+
+
     }
 }
