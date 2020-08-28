c176857cdaec7e45bc584498dc5665d1dfd1f666
sreeja <sreejasridhar03@gmail.com>
2020-08-28 19:29:52 +0530

    Login Draft
    Signed-off-by: sreeja <sreejasridhar03@gmail.com>

--- FindYourself/app/src/main/java/com/findyourself/MainActivity.java
+++ FindYourself/app/src/main/java/com/findyourself/MainActivity.java
@@ -1,19 +1,41 @@
 package com.findyourself;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button b;
+    EditText user;
+    EditText pass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("findyourself", "finnnd");
+        b = (Button) findViewById(R.id.button1);
+        user = (EditText) findViewById(R.id.editText1);
+        pass = (EditText) findViewById(R.id.editText2);
 
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user.getText().toString().equals("admin") &&
+                        pass.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
     }
+
 }
