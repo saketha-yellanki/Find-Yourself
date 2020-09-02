b7a15d2aa99b2cffbe3704f14040931d1d9bcbc0
“SakethaYellanki“ <sakethayellanki05@gmail.com>
2020-09-02 13:46:20 +0530

    User Authentication donee
    Signed-off-by: “SakethaYellanki“ <sakethayellanki05@gmail.com>

--- FindYourself/app/src/main/java/com/findyourself/RegisterActivity.java
+++ FindYourself/app/src/main/java/com/findyourself/RegisterActivity.java
@@ -2,27 +2,36 @@ package com.findyourself;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Calendar;
 import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    TextInputLayout username, fullname, password, conf_password, dob;
-    TextInputEditText un_et, fn_et, pass_et, conf_pass_et, dob_et;
+    TextInputLayout username, fullname, password, conf_password, dob, email;
+    TextInputEditText un_et, fn_et, pass_et, conf_pass_et, dob_et, email_et;
     RadioGroup gender_grp;
     MaterialButton register_btn;
     int dob_year, dob_month, dob_day;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +41,25 @@ public class RegisterActivity extends AppCompatActivity {
         findViews();
         implementDatePicker();
 
+        mAuth = FirebaseAuth.getInstance();
+
         register_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (isError()) {
                     errorImplementation();
                 } else {
-                    String username = Objects.requireNonNull(un_et.getText()).toString();
-                    String fullname = Objects.requireNonNull(fn_et.getText()).toString();
-                    String password = Objects.requireNonNull(pass_et.getText()).toString();
-                    String conf_password = Objects.requireNonNull(conf_pass_et.getText()).toString();
+                    String un = Objects.requireNonNull(un_et.getText()).toString();
+                    String fn = Objects.requireNonNull(fn_et.getText()).toString();
+                    String em = Objects.requireNonNull(email_et.getText()).toString();
+                    String pass = Objects.requireNonNull(pass_et.getText()).toString();
+                    String conf_pass = Objects.requireNonNull(conf_pass_et.getText()).toString();
                     String birthday = Objects.requireNonNull(dob_et.getText()).toString();
 
-                    if (passwordValid(password, conf_password)) {
+                    if (passwordValid(pass, conf_pass)) {
                         if (getAge(dob_year, dob_month, dob_day) >= 15) {
                             //register user
+                            createUser(em, pass);
 
                         } else {
                             Toast.makeText(RegisterActivity.this, "You are under age to use this application", Toast.LENGTH_SHORT).show();
@@ -60,6 +73,28 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void createUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "createUserWithEmail:success");
+                            Toast.makeText(RegisterActivity.this, "User created", Toast.LENGTH_SHORT).show();
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
     }
 
     private int getAge(int year, int month, int day) {
@@ -82,7 +117,7 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private boolean isError() {
-        return un_et.length() == 0 || fn_et.length() == 0 || pass_et.length() == 0 || conf_pass_et.length() == 0 || dob_et.length() == 0;
+        return un_et.length() == 0 || fn_et.length() == 0 || pass_et.length() == 0 || conf_pass_et.length() == 0 || dob_et.length() == 0 || email_et.length() == 0;
     }
 
     private void errorImplementation() {
@@ -101,6 +136,9 @@ public class RegisterActivity extends AppCompatActivity {
         }
         if (dob_et.length() == 0) {
             dob_et.setError("Date of Birth cannot be empty");
+        }
+        if (email_et.length() == 0) {
+            email_et.setError("Email cannot be empty");
         }
 
     }
@@ -147,6 +185,8 @@ public class RegisterActivity extends AppCompatActivity {
         conf_pass_et = findViewById(R.id.reg_conf_pass_et);
         dob_et = findViewById(R.id.date_et);
         register_btn = findViewById(R.id.reg_btn);
+        email = findViewById(R.id.reg_email);
+        email_et = findViewById(R.id.reg_email_et);
 
 
     }
