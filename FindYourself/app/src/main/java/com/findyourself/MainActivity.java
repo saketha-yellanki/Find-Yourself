3f98a9b756827455774dff49f10a35538271e3e3
“SakethaYellanki“ <sakethayellanki05@gmail.com>
2020-09-16 21:57:06 +0530

    login draft 4(retrive data)
    Signed-off-by: sreeja <sreejasridhar03@gmail.com>

--- FindYourself/app/src/main/java/com/findyourself/MainActivity.java
+++ FindYourself/app/src/main/java/com/findyourself/MainActivity.java
@@ -1,15 +1,22 @@
 package com.findyourself;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -23,12 +30,16 @@ import java.util.Objects;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth fAuth;
+    FirebaseAuth.AuthStateListener fAuthListener;
     FirebaseUser current_user;
     FirebaseDatabase db;
     ThisUser user;
 
     TextView textView;
     MaterialButton logout_btn;
+    MaterialButton login_btn;
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +55,27 @@ public class MainActivity extends AppCompatActivity {
 
         //Log.i("Firebase User Details",current_user.getUid());
         //Log.i("Firebase User Details",Objects.requireNonNull(db.getReference("users").child(current_user.getUid()).toString()));
+        fAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                    Intent intent = new Intent(MainActivity.this, ChatRoomActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+
+        };
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSignIn();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
         logout_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,9 +87,45 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @SuppressLint("WrongViewCast")
     private void findViews() {
         logout_btn = findViewById(R.id.logout);
         textView = findViewById(R.id.text);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login_btn = findViewById(R.id.login);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fAuth.addAuthStateListener(fAuthListener);
+    }
+
+    private void startSignIn() {
+        String user_string = username.getText().toString();
+        String pass_string = password.getText().toString();
+
+        if (TextUtils.isEmpty(user_string) || TextUtils.isEmpty(pass_string)) {
+            Toast.makeText(MainActivity.this, "Fields are empty", Toast.LENGTH_LONG).show();
+
+        } else {
+            fAuth.signInWithEmailAndPassword(user_string, pass_string).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Successfully Signed In", Toast.LENGTH_LONG).show();
+                        // val intent = Intent(this, MainActivity::class.java);
+                        //startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Sign-in Failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+
+
     }
 
     private void loadData() {
