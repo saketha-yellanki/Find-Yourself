9e0da61fa5e4ac522e3da8baa1f572c6705810a5
“SakethaYellanki“ <sakethayellanki05@gmail.com>
2020-09-20 13:10:01 +0530

    Fixed Login and register activities
    Signed-off-by: “SakethaYellanki“ <sakethayellanki05@gmail.com>

--- FindYourself/app/src/main/java/com/findyourself/activities/LoginActivity.java
+++ FindYourself/app/src/main/java/com/findyourself/activities/LoginActivity.java
@@ -1,18 +1,100 @@
 package com.findyourself.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.findyourself.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    MaterialButton login_btn;
+    TextInputEditText email;
+    TextInputEditText password;
+    MaterialTextView new_user;
+
+    FirebaseAuth fAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        findViews();
+
+        fAuth = FirebaseAuth.getInstance();
+
+        new_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSignIn();
+            }
+        });
+    }
+
+    private void findViews() {
+        email = findViewById(R.id.email_login_et);
+        password = findViewById(R.id.pass_login_et);
+        login_btn = findViewById(R.id.login);
+        new_user = findViewById(R.id.new_user_txt);
+    }
+
+    private void startSignIn() {
+        String user_string = Objects.requireNonNull(email.getText()).toString();
+        String pass_string = Objects.requireNonNull(password.getText()).toString();
+
+        if (TextUtils.isEmpty(user_string) || TextUtils.isEmpty(pass_string)) {
+            if (TextUtils.isEmpty(user_string)) {
+                email.setError("Email is required");
+            }
+            if (TextUtils.isEmpty(pass_string)) {
+                password.setError("Password is required");
+            }
+
+        } else {
+            fAuth.signInWithEmailAndPassword(user_string, pass_string).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Successfully Signed In", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Sign-in Failed", Toast.LENGTH_LONG).show();
+                        Log.i("Authentication Failed", task.getException().toString());
+                    }
+                }
+            });
+        }
+
+
     }
 }
