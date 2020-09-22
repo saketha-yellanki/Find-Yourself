c86df1f9126fd7365a462c23101357c511d4ed20
harshithav5 <harshithav5600@gmail.com>
2020-09-22 18:33:48 +0530

    yourchat
    Signed-off-by: harshithav5 <harshithav5600@gmail.com>

--- FindYourself/app/src/main/java/com/findyourself/fragments/YourChatsFragment.java
+++ FindYourself/app/src/main/java/com/findyourself/fragments/YourChatsFragment.java
@@ -1,65 +1,112 @@
 package com.findyourself.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.fragment.app.Fragment;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.findyourself.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link YourChatsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
 public class YourChatsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView dataList;
+    List<String> titles;
+    RecyclerView.Adapter adapter;
 
     public YourChatsFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment YourChatRooms.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static YourChatsFragment newInstance(String param1, String param2) {
-        YourChatsFragment fragment = new YourChatsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_your_chat_rooms, container, false);
+        View view = inflater.inflate(R.layout.fragment_your_chat_rooms, container, false);
+        dataList = view.findViewById(R.id.your_chat_rv);
+        titles = new ArrayList<>();
+
+
+        titles.add("First Item");
+        titles.add("Second Item");
+        titles.add("Third Item");
+        titles.add("Fourth Item");
+        titles.add("Fifth Item");
+        titles.add("Sixth Item");
+
+
+        adapter = new YourChatsFragment.Adapter(getContext(), titles);
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        dataList.setLayoutManager(linearLayoutManager);
+        dataList.setAdapter(adapter);
+
+
+        return view;
+
     }
+
+    public class Adapter extends RecyclerView.Adapter<YourChatsFragment.Adapter.ViewHolder> {
+        List<String> titles;
+        LayoutInflater inflater;
+
+        public Adapter(Context ctx, List<String> titles) {
+            this.titles = titles;
+            this.inflater = LayoutInflater.from(ctx);
+        }
+
+        @NonNull
+        @Override
+        public YourChatsFragment.Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = inflater.inflate(R.layout.custom_list_layout, parent, false);
+            return new YourChatsFragment.Adapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull YourChatsFragment.Adapter.ViewHolder holder, int position) {
+            holder.title.setText(titles.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return titles.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView title;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                title = itemView.findViewById(R.id.textview3);
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(v.getContext(), "Clicked -> " + getAdapterPosition(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+    }
+
 }
